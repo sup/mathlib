35b4a0886d28d20d6dacf616208fa289639e7cdc
cjl223 <cjl223@cornell.edu>
2013-08-21 13:44:17 -0500

    add skeleton for RSA and Caesar Cipher

--- mathlib/numbers/crypto.py
+++ mathlib/numbers/crypto.py
@@ -13,3 +13,15 @@ These include but are not limited to: RSA Encryption, One-Time-Pad
 Encryption, Ceaser Cipher, and various hashing algorithms. Part of the
 MathLib/numbers package.
 """
+
+def ceasar_cipher(message, key):
+	pass
+
+def RSA_encrpyt(public_key, private_key, message):
+	pass
+
+def RSA_decrypt(public_key, private_key, cipher):
+	pass
+
+def phi(number):
+	pass
