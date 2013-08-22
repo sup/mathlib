70a03a1a67f6d72d8241a21fcb1a9ef1b738b702
cjl223 <cjl223@cornell.edu>
2013-08-21 21:37:01 -0500

    overhaul matrix.py, see extended description
    * Update documentation/comments
    * Use Matlab style matrix syntax for construction and matrix coordinates
    * Remove _number_of_rows/_number_of_columns/_row_length/_col_length with
    m and n for rows and columns resepectively
    * Add helpers for parsing input in constructors and setters as well as
    checking if the input is valid with _parse() and _check()
    * Implemented individual set and get functions for single data points in
    the matrix
    * Add skeleton for fliplr() -> reversing arrays/rows/columns

--- mathlib/core/matrix.py
+++ mathlib/core/matrix.py
@@ -16,13 +16,31 @@ implemented as cases within the appropriate functions in the unimath (core
 math functions) module for Matrix class types as well as overloaded 
 operators defined within the Matrix class.
 
+Usage/Syntax
+============
+Creating a matrix using a constructor or setter requires the use of a 
+Matlab-like syntax. For example, a 3*3 matrix:
+
+								1 2 3
+								3 2 1
+								5 1 4
+		
+has a raw string expression using Matlab syntax: "1,2,3;3,2,1;5,1,4"
+with no spaces, using semi-colons to separate rows, and commas to
+separate elements within each row. X and Y coordinates also mimic
+Matlab's non-zero coordinate system for traversing a matrix.
+
 Contents
 --------
 * Array 	   - A one-dimensional array class with various methods
 * Matrix 	   - A matrix class with various methods
 * fliplr() 	   - Reverses an array or column of a matrix
+* ref() 	   - Returns the row echelon form of a matrix
+* rref() 	   - Returns the reduced row echelon form of a matrix
+* trans() 	   - Returns the transpose of a matrix or array
+* det() 	   - Returns the determinant of a matrix
+* diag() 	   - Returns the diagonalized form of a matrix
 * eig() 	   - Returns the eigenvector of a matrix
-* transpose()  - Returns the transpose of a matrix or array
 """
 
 class Matrix(object):
@@ -30,70 +48,43 @@ class Matrix(object):
 	"""
 	#Properties
 	_matrix = None
-	_row_length = 0
-	_col_length = 0
-	_number_of_rows = 0
-	_number_of_columns = 0
+	_m = 0
+	_n = 0
 	_is_square = False
-	_is_li = False
 
 	@property 
 	def matrix(self):
 		return self._matrix
 
 	@matrix.setter
-	def matrix(self, pre_matrix):
-		assert type(pre_matrix) == list, "Argument given is not a 2D-List"
-		assert _has_same_length_rows(pre_matrix) == True, "Argument has different length rows"
-		self._matrix = pre_matrix
+	def matrix(self, raw_matrix):
+		self._matrix = raw_matrix
 
 	@property #immutable
-	def row_length(self):
-		return self._row_length
+	def m(self):
+		return self._m
 
 	@property #immutable
-	def col_length(self):
-		return self._col_length
-
-	@property #immutable
-	def number_of_rows(self):
-		return self._number_of_rows
-
-	@property #immutable
-	def number_of_columns(self):
-		return self._number_of_columns
+	def n(self):
+		return self._n
 
 	@property #immutable
 	def is_square(self):
 		return self._is_square
 
-	@property #immutable
-	def is_li(self):
-		return self._is_li
+	#Constructor
+	def __init__(self, raw_matrix):
+		"""
+		Constructor: Create a new instance of a Matrix
+        
+        Precondition: raw_matrix is a valid matrix input according to the
+        description in the meta documentation.
+		"""
+		#Set and check the matrix
+		self._parse(raw_matrix)
+		self._check()
 
-	#Methods
-	def __init__(self, pre_matrix):
-		"""
-		"""
-		#Test if the "pre_matrix" argument is valid
-		assert type(pre_matrix) == list, "Argument given is not a 2D-List"
-		assert _has_same_length_rows(pre_matrix) == True, "Argument has different length rows"
-		#Set the matrix
-		self._matrix = pre_matrix
-		#Set row/col length/numbers
-		self._number_of_rows = len(self._matrix)
-		elf._number_of_columns = len(self._matrix[0])
-		self._col_length = self._number_of_rows
-		self._row_length = self._number_of_columns
-		#Set whether the matrix is a square matrix
-		if self._number_of_rows == self._number_of_columns:
-			self._is_square == True
-
-	def __str__(self):
-		"""
-		"""
-		pass
-
+	#Built-In Methods/Operator Overloading
 	def __add__(self):
 		"""
 		"""
@@ -113,34 +104,106 @@ class Matrix(object):
 		"""
 		"""
 		pass
+
+	#Methods Proper
+	def disp(self, heading = "Matrix:"):
+		"""
+		Procedure: Prints a string representation of the matrix object
+		"""
+		print heading
+		#Iteratively print the elements of the matrix
+        for x in range(0, self._m):
+            for y in range(0, self._n):
+                print '%10.3f' % self._matrix[x][y],
+        #Print an empty line
+		print ""
+
+	def get(self, x, y):
+		"""
+		Returns: Value a specified coordinate point within the matrix.
+
+		Precondition: x and y are valid coordinates using Matlab coordinate
+		rules.
+		"""
+		return self._matrix[x-1][y-1]
+
+	def set(self, x, y, value):
+		"""
+		Procedure: Sets value at a specfied coordinate point to a new value.
+
+		Precondition: x and y are valid coordinates using Matlab coordinate
+		rules.
+		"""
+		#Check if the value specified is a string or an actual list.
+		assert type(value) == float or type(value) == int
+		self._matrix[x-1][y-1] = value
+
+	def add_row(self, row):
+		"""
+		Procedure: Adds a given row to the matrix
+
+		Preconditions: The row must be have the same length as other rows in
+		the matrix. Argument passed to row is a python list.
+		"""
+		assert len(row) == self._row_length, "The row has an invalid length"
+
+	def add_col(self, col):
+		"""
+		Procedure: Adds a given col to the matrix
+		"""
+		pass
+
+	def sub_row(self, row_number):
+		"""
+		Procedure: Subtracts a given row from the matrix by row number
+		"""
+		pass
+
+	def sub_col(self, col_number):
+		"""
+		Procedure: Subtracts a given column from the matrix by column number
+		"""
+		pass
+
+	#Helper Methods
+	def _parse(self, raw_matrix):
+		"""
+		Procedure: Parses the raw_matrix string and updates the Matrix object.
+
+		Precondition: raw_matrix is a valid input according to the description
+		in the meta documentation at the top of the document.
+		"""
+		#Initialize default values
+		self._m = -1
+		self._n = -1
+		#Split the matrix up into rows of strings and set m to len(self._matrix
+		self._matrix = raw_matrix.split(';')
+		self._m = len(self._matrix)
+		#Continue parsing the input, make rows into lists of numbers
+		for x in range(0, self._m):
+			self._matrix[x] = self.matrix[x].split(",")
+			self._matrix[x] = list(map(float, self.matrix[x]))
+		#Set n to the number of elements in each row
+		self._n = len(self._matrix[0])
+		#Set whether the matrix is a square matrix
+		if self._m == self._n:
+			self._is_square == True
+
+
+	def _check(self):
+		"""
+		Procedure: Checks if the raw input was valid by QCing the properties
+		of the matrix.
+		"""
+		#Test if the rest of the rows are the same length
+		for r in range(0, len(self._m)):
+			assert len(self._matrix[r]) == self._n, "Invalid row lengths"
+			assert type(self._matrix[r]) == list, "Some rows aren't lists"
+
 #======================
 #	Matrix Functions
 #======================
-
-def add_row(matrix, row):
-	"""
-	Procedure: Adds a given row to a specified matrix
-
-	Preconditions: The row must be have the same length as other rows in
-	the matrix.
-	"""
-	assert len(row) == matrix.row_length, "The row has an invalid length"
-	pass
-
-
-def add_col(matrix, col):
-	"""
-	"""
-	pass
-
-
-def sub_row(matrix, row_number):
-	"""
-	"""
-	pass
-
-
-def sub_col(matrix, col_number):
+def fliplr(array):
 	"""
 	"""
 	pass
@@ -194,16 +257,4 @@ def is_li(matrix):
 	"""
 	"""
 	pass
-
-
-def _has_same_length_rows(pre_matrix):
-	"""
-	"""
-	#Check the first row
-	first_row_length = len(pre_matrix[0])
-	#Test if the rest of the rows are the same length
-	for r in range(1, len(pre_matrix)):
-		if len(pre_matrix[r]) != first_row_length:
-			return False
-	return True
 
