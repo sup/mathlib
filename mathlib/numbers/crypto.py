802ad83720af364e2457346bc938030b46cebd93
Charles Lai <cjl223@cornell.edu>
2014-01-19 10:48:26 -0600

    add caeser cipher method

--- mathlib/numbers/crypto.py
+++ mathlib/numbers/crypto.py
@@ -14,8 +14,37 @@ Encryption, Ceaser Cipher, and various hashing algorithms. Part of the
 MathLib/numbers package.
 """
 
-def ceasar_cipher(message, key):
-    pass
+
+def caeser(message, key):
+	"""
+	Returns: Ciphertext of message encrypted with a Caeser cipher. This
+	encryption function preserves case, whitespace, and punctuation.
+	"""
+	#Raw Alphabets
+	low_alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+				 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
+
+	cap_alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+				 "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]
+
+	#Dictionaries for each alphabet type
+	low_dict = {}
+	cap_dict = {}
+	for i in range(26):
+		low_dict[low_alpha[i]] = low_alpha[(i+key) % 26]
+		cap_dict[cap_alpha[i]] = cap_alpha[(i+key) % 26]
+
+	#Conversion of message to ciphertext
+	ciphertext = ""
+	for l in message:
+		if l in low_dict:
+			l = low_dict[l]
+		if l in cap_dict:
+			l = cap_dict[l]
+		ciphertext += l
+
+	return ciphertext
+
 
 def RSA_encrpyt(public_key, private_key, message):
     pass
